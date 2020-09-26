5afdc7da82635f9c585fbae99a1e321d6648bcbc
Denis_illarionov <denisill90@yandex.ru>
2020-09-27 01:40:49 +0300

    Create ContactData.java

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstname;
+    private final String lastname;
+    private final String hometelefon;
+    private final String email;
+
+    public ContactData(String firstname, String lastname, String hometelefon, String email) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.hometelefon = hometelefon;
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getHometelefon() {
+        return hometelefon;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}

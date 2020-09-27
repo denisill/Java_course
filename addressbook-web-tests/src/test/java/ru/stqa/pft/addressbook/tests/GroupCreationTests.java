f46017d1ea7c6c13c7202ed67e9a32aaac6eac77
Denis_illarionov <denisill90@yandex.ru>
2020-09-28 00:46:28 +0300

    Update GroupCreationTests.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -9,10 +9,10 @@ public class GroupCreationTests extends TestBase {
   @Test
   public void testGroupCreation() throws Exception {
 
-    app.fillGroupForm(new GroupData("test11", "test2", "test3"));
-    app.submitGroupCreation();
-    app.returnToGroupPage();
-    app.logout();
+    app.getGroupHelper().fillGroupForm(new GroupData("test11", "test2", "test3"));
+    app.getGroupHelper().submitGroupCreation();
+    app.getGroupHelper().returnToGroupPage();
+    app.getSessionHelper().logout();
   }
 
 

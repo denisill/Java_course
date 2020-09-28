5641dbf3ddd8921ea9afe4e9149040c55aae391f
Denis_illarionov <denisill90@yandex.ru>
2020-09-29 00:37:07 +0300

    Update NavigationHelper.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -10,14 +10,18 @@ public class NavigationHelper extends HelperBase {
     }
 
     public void gotoContactPage() {
-      click(By.linkText("add new"));
+        click(By.linkText("add new"));
     }
 
     public void gotoGroupPage() {
-      click(By.linkText("groups"));
+        click(By.linkText("groups"));
     }
 
     public void returnToHomePage() {
-      click(By.linkText("home page"));
+        click(By.linkText("home page"));
+    }
+
+    public void gotoContactList() {
+        click(By.linkText("home"));
     }
 }

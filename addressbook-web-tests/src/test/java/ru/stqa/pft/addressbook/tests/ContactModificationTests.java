ac6d103fa638f1059198132f30e92d90bf61e9a1
Denis_illarionov <denisill90@yandex.ru>
2020-10-07 10:56:22 +0300

    Добавление предусловий для модификации контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -8,6 +8,11 @@ public class ContactModificationTests extends TestBase {
     @Test
     public void testContactModification() {
         app.getNavigationHelper().gotoContactList();
+            if (!app.getContactHelper().isThereAContact()) {
+                app.getContactHelper().initContactCreation();
+                app.getContactHelper().createContact(new ContactData("name1", null, null, null));
+                app.getNavigationHelper().returnToHomePage();
+        }
         app.getContactHelper().selectContact();
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactData("Denis", "Illarionov", "8(495)0010000", "denisill@mail.ru"));

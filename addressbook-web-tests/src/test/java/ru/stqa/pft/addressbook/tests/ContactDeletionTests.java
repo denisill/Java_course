29e30e8ce853651d43190d0edd7fb63c99bcb4c1
Denis_illarionov <denisill90@yandex.ru>
2020-10-07 10:56:08 +0300

    Добавление предусловий для удаления контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,12 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactDeletionTests extends TestBase {
 
     @Test
     public void testContactDeletion() {
         app.getNavigationHelper().gotoContactList();
+            if (! app.getContactHelper().isThereAContact()){
+                app.getContactHelper().initContactCreation();
+                app.getContactHelper().createContact(new ContactData("name1", null, null, null ));
+                app.getNavigationHelper().returnToHomePage();
+            }
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteSelectedContacts();
         app.getContactHelper().closeAlert();

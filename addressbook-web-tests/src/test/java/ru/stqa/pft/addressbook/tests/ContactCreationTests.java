0bd8a87db0f7eaa2f28a6b2cf6b3ce5cab9f87c9
Denis_illarionov <denisill90@yandex.ru>
2020-10-07 10:53:26 +0300

    Update ContactCreationTests.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -9,8 +9,7 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() throws Exception {
 
     app.getNavigationHelper().gotoContactPage();
-    app.getContactHelper().fillContactForm(new ContactData("Denis", "Illarionov", "8(495)0010000", "denisill@mail.ru"));
-    app.getContactHelper().submitContactCreation();
+    app.getContactHelper().createContact(new ContactData("Denis", "Illarionov", "8(495)0010000", "denisill@mail.ru"));
     app.getNavigationHelper().returnToHomePage();
     app.getSessionHelper().logout();
   }

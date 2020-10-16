3faf49625f00a71f3b2c09ed29d7dda817198360
Denis_illarionov <denisill90@yandex.ru>
2020-10-16 11:03:11 +0300

    Update ContactCreationTests.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -8,10 +9,15 @@ public class ContactCreationTests extends TestBase {
   @Test
   public void testContactCreation() throws Exception {
 
+    app.getNavigationHelper().gotoContactList();
+    int before = app.getContactHelper().getContactCount();
     app.getNavigationHelper().gotoContactPage();
     app.getContactHelper().createContact(new ContactData("Denis", "Illarionov", "8(495)0010000", "denisill@mail.ru"));
     app.getNavigationHelper().returnToHomePage();
+    int after = app.getContactHelper().getContactCount();
     app.getSessionHelper().logout();
+
+    Assert.assertEquals(after, before + 1);
   }
 
 

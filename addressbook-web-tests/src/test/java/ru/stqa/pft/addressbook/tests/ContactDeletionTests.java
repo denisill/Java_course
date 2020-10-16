f902b3b0e55e10a7ccbc4d20d3e746f663978815
Denis_illarionov <denisill90@yandex.ru>
2020-10-16 11:04:05 +0300

    Тест по удалению контакта, проверка на кол-во

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
@@ -8,13 +9,18 @@ public class ContactDeletionTests extends TestBase {
     @Test
     public void testContactDeletion() {
         app.getNavigationHelper().gotoContactList();
-            if (! app.getContactHelper().isThereAContact()){
-                app.getContactHelper().initContactCreation();
-                app.getContactHelper().createContact(new ContactData("name1", null, null, null ));
-                app.getNavigationHelper().returnToHomePage();
-            }
+        if (!app.getContactHelper().isThereAContact()) {
+            app.getContactHelper().initContactCreation();
+            app.getContactHelper().createContact(new ContactData("name1", null, null, null));
+            app.getNavigationHelper().returnToHomePage();
+        }
+        int before = app.getContactHelper().getContactCount();
         app.getContactHelper().selectContact();
         app.getContactHelper().deleteSelectedContacts();
         app.getContactHelper().closeAlert();
+        app.getNavigationHelper().gotoContactList();
+        int after = app.getContactHelper().getContactCount();
+
+        Assert.assertEquals(after, before - 1);
     }
 }

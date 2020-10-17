9b4515f09252592011eeb442eb6dc590ccda7884
Denis_illarionov <denisill90@yandex.ru>
2020-10-17 17:48:45 +0300

    Update ContactDeletionTests.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -15,10 +15,9 @@ public class ContactDeletionTests extends TestBase {
             app.getNavigationHelper().returnToHomePage();
         }
         int before = app.getContactHelper().getContactCount();
-        app.getContactHelper().selectContact();
+        app.getContactHelper().selectContact(before - 1);
         app.getContactHelper().deleteSelectedContacts();
         app.getContactHelper().closeAlert();
-        app.getNavigationHelper().gotoContactList();
         int after = app.getContactHelper().getContactCount();
 
         Assert.assertEquals(after, before - 1);

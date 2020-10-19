5d759562364b62b8f4c35be811b461890beaae86
Denis_illarionov <denisill90@yandex.ru>
2020-10-19 19:11:44 +0300

    Тест для создания контакта, упорядоченные списки перед сравнением

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,23 +4,33 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
-  @Test
-  public void testContactCreation() throws Exception {
+    @Test
+    public void testContactCreation() throws Exception {
 
-    app.getNavigationHelper().gotoContactList();
-    List<ContactData> before = app.getContactHelper().getContactList();
-    app.getNavigationHelper().gotoContactPage();
-    app.getContactHelper().createContact(new ContactData("Denis", "Illarionov", "8(495)0010000", "denisill@mail.ru"));
-    app.getNavigationHelper().returnToHomePage();
-    List<ContactData> after = app.getContactHelper().getContactList();
-    app.getSessionHelper().logout();
+        app.getNavigationHelper().gotoContactList();
+        List<ContactData> before = app.getContactHelper().getContactList();
+        app.getNavigationHelper().gotoContactPage();
+        ContactData contact = new ContactData("Denis", "Illarionov", "8(495)0010000", "denisill@mail.ru");
+        app.getContactHelper().createContact(contact);
+        app.getNavigationHelper().returnToHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        app.getSessionHelper().logout();
+        Assert.assertEquals(after.size(), before.size() + 1);
 
-    Assert.assertEquals(after.size(), before.size() + 1);
-  }
+
+        before.add(contact);
+        Comparator<? super ContactData> byId = (c1, c2) -> Integer.compare(c1.getId(), c2.getId());
+        before.sort(byId);
+        after.sort(byId);
+        Assert.assertEquals(before, after);
+
+    }
 
 
 }

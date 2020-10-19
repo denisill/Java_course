fabe9c91a450c638fe45db4858ee4dde5ab88e05
Denis_illarionov <denisill90@yandex.ru>
2020-10-19 19:09:38 +0300

    Update ContactData.java

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,16 +1,52 @@
 package ru.stqa.pft.addressbook.model;
 
 public class ContactData {
+    private int id;
     private final String firstname;
     private final String lastname;
     private final String hometelefon;
     private final String email;
 
-    public ContactData(String firstname, String lastname, String hometelefon, String email) {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContactData that = (ContactData) o;
+
+        if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+        return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstname != null ? firstname.hashCode() : 0;
+        result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+        return result;
+    }
+
+    public ContactData(int id, String firstname, String lastname, String hometelefon, String email) {
+        this.id = id;
         this.firstname = firstname;
         this.lastname = lastname;
         this.hometelefon = hometelefon;
         this.email = email;
+    }
+    public ContactData(String firstname, String lastname, String hometelefon, String email) {
+        this.id = Integer.MAX_VALUE;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.hometelefon = hometelefon;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "id='" + id + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                '}';
     }
 
     public String getFirstname() {
@@ -27,5 +63,13 @@ public class ContactData {
 
     public String getEmail() {
         return email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }

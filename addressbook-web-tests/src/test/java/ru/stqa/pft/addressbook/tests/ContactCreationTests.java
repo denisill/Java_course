1121ed4d5e026e3be930b0d4e40d6f2c0a917bce
Denis_illarionov <denisill90@yandex.ru>
2020-10-30 00:55:13 +0300

    Задание №12-13

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,28 +1,48 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
 
-    @Test
-    public void testContactCreation() throws Exception {
+    @DataProvider
+    public Iterator<Object[]> validContacts() throws IOException {
+
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(ContactData.class);
+        List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+        return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+
+    @Test(dataProvider = "validContacts")
+    public void testContactCreation(ContactData contact) throws Exception {
 
         app.goTo().contactPage();
         Contacts before = app.contact().all();
         app.goTo().contactAdd();
-        File photo = new File("src/test/resources/stru.png");
-        ContactData contact = new ContactData().withFirstname("Denis").withLastname("Ill").withPhoto(photo);
         app.contact().create(contact);
         app.goTo().homePage();
         Contacts after = app.contact().all();
-        app.getSessionHelper().logout();
         assertThat(after.size(), equalTo(before.size() + 1));
 
         assertThat(after, equalTo(before.withAdded(
@@ -30,14 +50,6 @@ public class ContactCreationTests extends TestBase {
 
     }
 
-    /*@Test
-    public void testCurrentDir() {
-        File currentDir = new File(".");
-        System.out.println(currentDir.getAbsolutePath());
-        File photo = new File("src/test/resources/stru.png");
-        System.out.println(photo.getAbsolutePath());
-        System.out.println(photo.exists());
-    }*/
 
 
 
